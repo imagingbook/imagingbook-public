c19d02cb3fefb26b5ba5ca205f4a8cc6bb305de9
imagingbook <wilbur@ieee.org>
2022-06-25 10:23:42 +0200

    added Delaunay_Demo plugin to Extras

---
+++ imagingbook-plugins-extras/src/main/java/Delaunay_Triangulation/Delaunay_Demo.java
@@ -0,0 +1,125 @@
+package Delaunay_Triangulation;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.util.List;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.gui.GenericDialog;
+import ij.gui.Overlay;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageProcessor;
+import imagingbook.common.corners.Corner;
+import imagingbook.common.corners.HarrisCornerDetector;
+import imagingbook.common.geometry.basic.Pnt2d;
+import imagingbook.common.geometry.delaunay.DelaunayTriangulation;
+import imagingbook.common.geometry.delaunay.Triangle;
+import imagingbook.common.ij.overlay.ColoredStroke;
+import imagingbook.common.ij.overlay.ShapeOverlayAdapter;
+
+
+/**
+ * This plugin runs the Harris corner detector on the current image, 
+ * applies Delaunay triangulation to the N strongest corners and displays
+ * the result as a vector overlay in a new image.
+ * 
+ * @author W. Burger
+ * @version 2022/06/25
+ * @see HarrisCornerDetector
+ * @see DelaunayTriangulation
+ */
+public class Delaunay_Demo implements PlugInFilter {
+	
+	private static int CornerCount = 0;					// number of corners to show (0 = show all)
+	private static boolean ShuffleCorners = true;	
+	
+	private static Color DelaunayColor = Color.green;	// color for graph edges
+	private static Color PointColor = Color.red;		// color for point markers
+	
+	private static double StrokeWidth = 0.25;
+	private static double PointRadius = 1.0;
+	
+	private static HarrisCornerDetector.Parameters params = new HarrisCornerDetector.Parameters();
+	
+
+	private ImagePlus im = null;
+	
+    public int setup(String arg, ImagePlus im) {
+    	this.im = im;
+        return DOES_ALL + NO_CHANGES;
+    }
+    
+    public void run(ImageProcessor ip) {
+    	
+		if (!showDialog()) {
+			return;
+		}
+		
+		HarrisCornerDetector cd = new HarrisCornerDetector(ip, params);
+		List<Corner> corners = cd.getCorners();
+
+		DelaunayTriangulation dt = DelaunayTriangulation.from(corners, ShuffleCorners);
+		
+		ImageProcessor cp = ip.convertToByteProcessor();
+		Overlay oly = makeOverlay(dt);
+
+		ImagePlus im2 = new ImagePlus(im.getShortTitle() + "-delaunay", cp);
+		im2.setOverlay(oly);
+		im2.show();
+    }
+    
+	private boolean showDialog() {
+		GenericDialog dlg = new GenericDialog(this.getClass().getSimpleName());
+		dlg.addNumericField("Corner score threshold", params.scoreThreshold, 0);
+		dlg.addCheckbox("Clean up corners", params.doCleanUp);
+		dlg.addNumericField("Corners to show (0 = show all)", CornerCount, 0);
+		dlg.addCheckbox("Shuffle corners", ShuffleCorners);
+		
+		dlg.showDialog();
+		if(dlg.wasCanceled())
+			return false;	
+		
+		params.scoreThreshold = (int) dlg.getNextNumber();
+		params.doCleanUp = dlg.getNextBoolean();
+		CornerCount = (int) dlg.getNextNumber();
+		ShuffleCorners = dlg.getNextBoolean();
+		
+		if(dlg.invalidNumber() || !params.validate()) {
+			IJ.error("Input Error", "Invalid input");
+			return false;
+		}	
+		return true;
+	}
+	
+	// ---------------------------------------------------------------------------
+	
+	private Overlay makeOverlay(DelaunayTriangulation dt) {
+		List<Triangle> triangles = dt.getTriangles();
+		List<Pnt2d> allPoints = dt.getPoints();
+		
+		Overlay oly = new Overlay();
+		ShapeOverlayAdapter ola = new ShapeOverlayAdapter(oly);
+
+		// draw the triangles:
+		ColoredStroke triangleStroke = new ColoredStroke(StrokeWidth, DelaunayColor);
+		triangleStroke.setEndCap(BasicStroke.CAP_ROUND);
+		triangleStroke.setLineJoin(BasicStroke.JOIN_ROUND);
+		
+		ola.setStroke(triangleStroke);
+		for (Triangle trgl : triangles) {
+			 ola.addShape(trgl.getShape());
+		}
+		
+		// draw the vertices of the triangulation:
+		ColoredStroke pointStroke = new ColoredStroke(StrokeWidth, PointColor);
+		ola.setStroke(pointStroke);
+		for (Pnt2d p : allPoints) {
+			// get original Pnt2d shape (not Corner shape)
+			ola.addShape(Pnt2d.from(p).getShape(PointRadius));
+		}
+
+		return oly;
+	}
+	
+}
