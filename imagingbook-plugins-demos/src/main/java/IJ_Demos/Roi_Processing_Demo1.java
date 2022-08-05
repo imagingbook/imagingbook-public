feb1fb0eff0fe7f9c33a40c55e9b2bb1dc3f8600
imagingbook <wilbur@ieee.org>
2022-08-05 12:54:49 +0200

    renamed plugin package to 'IJ_Demos'

--- imagingbook-plugins-demos/src/main/java/IJ_Demos/Roi_Processing_Demo1.java
+++ imagingbook-plugins-demos/src/main/java/IJ_Demos/Roi_Processing_Demo1.java
@@ -6,7 +6,7 @@
  * Copyright (c) 2006-2022 Wilhelm Burger, Mark J. Burge. 
  * All rights reserved. Visit http://www.imagingbook.com for additional details.
  *******************************************************************************/
-package ImageJ_Examples;
+package IJ_Demos;
 
 import java.awt.Rectangle;
 
