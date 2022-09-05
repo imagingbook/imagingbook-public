8cb361614b3bb44b8aacfecb5b6a24549c1d813e
imagingbook <wilbur@ieee.org>
2022-09-05 11:18:35 +0200

    added 'LinearRgbColorSpace'

---
+++ imagingbook-common/src/main/java/imagingbook/common/color/colorspace/LinearRgbColorSpace.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ * This software is provided as a supplement to the authors' textbooks on digital
+ * image processing published by Springer-Verlag in various languages and editions.
+ * Permission to use and distribute this software is granted under the BSD 2-Clause 
+ * "Simplified" License (see http://opensource.org/licenses/BSD-2-Clause). 
+ * Copyright (c) 2006-2022 Wilhelm Burger, Mark J. Burge. 
+ * All rights reserved. Visit https://imagingbook.com for additional details.
+ *******************************************************************************/
+
+package imagingbook.common.color.colorspace;
+
+import java.awt.color.ColorSpace;
+
+
+/**
+ * Basic linear RGB color space, related to sRGB by gamma
+ * correction.
+ * Everything is D65, components of all {@code float[]} colors
+ * are supposed to be in [0,1].
+ * This is a singleton class with no public constructors,
+ * use {@link #getInstance()} to obtain the single instance.
+ * 
+ */
+@SuppressWarnings("serial")
+public class LinearRgbColorSpace extends ColorSpace {
+	
+	private static final LinearRgbColorSpace instance = new LinearRgbColorSpace();
+	
+	public static LinearRgbColorSpace getInstance() {
+		return instance;
+	}
+	
+	/**
+	 * Constructor.
+	 */
+	private LinearRgbColorSpace() {
+		super(ColorSpace.TYPE_RGB, 3);
+	}
+
+	@Override
+	public float[] fromRGB(float[] srgb) {
+		final float[] rgb = new float[3];
+		for (int k = 0; k < 3; k++) {
+			rgb[k] = (float) sRgbUtil.gammaInv(srgb[k]);
+		}
+		return rgb;
+	}
+
+	@Override
+	public float[] toRGB(float[] rgb) {
+		final float[] srgb = new float[3];
+		for (int k = 0; k < 3; k++) {
+			srgb[k] = (float) sRgbUtil.gammaFwd(rgb[k]);
+		}
+		return srgb;
+	}
+
+	@Override
+	public float[] toCIEXYZ(float[] colorvalue) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public float[] fromCIEXYZ(float[] colorvalue) {
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ imagingbook-common/src/test/java/imagingbook/common/color/colorspace/LinearRgbColorSpaceTest.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * This software is provided as a supplement to the authors' textbooks on digital
+ * image processing published by Springer-Verlag in various languages and editions.
+ * Permission to use and distribute this software is granted under the BSD 2-Clause 
+ * "Simplified" License (see http://opensource.org/licenses/BSD-2-Clause). 
+ * Copyright (c) 2006-2022 Wilhelm Burger, Mark J. Burge. 
+ * All rights reserved. Visit https://imagingbook.com for additional details.
+ *******************************************************************************/
+package imagingbook.common.color.colorspace;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.awt.color.ColorSpace;
+import java.util.Arrays;
+import java.util.Random;
+
+import org.junit.Test;
+
+public class LinearRgbColorSpaceTest {
+	
+	static ColorSpace cs = LinearRgbColorSpace.getInstance();
+
+	@Test
+	public void test1() {
+		doCheck(cs, new int[] {0, 0, 0});
+		doCheck(cs, new int[] {255, 255, 255});
+		doCheck(cs, new int[] {177, 0, 0});
+		doCheck(cs, new int[] {0, 177, 0});
+		doCheck(cs, new int[] {0, 0, 177});
+		doCheck(cs, new int[] {19, 3, 174});
+	}
+	
+	@Test
+	public void test2() {
+		Random rd = new Random(17);
+		for (int i = 0; i < 10000; i++) {
+			int r = rd.nextInt(256);
+			int g = rd.nextInt(256);
+			int b = rd.nextInt(256);
+			doCheck(cs, new int[] {r, g, b});
+		}
+	}
+	
+//	@Test	// tests all possible rgb combinations
+//	public void test3() {
+//		for (int r = 0; r < 256; r++) {
+//			for (int g = 0; g < 256; g++) {
+//				for (int b = 0; b < 256; b++) {
+//					doCheck(cs, new int[] {r, g, b});
+//				}
+//			}
+//		}
+//	}
+	
+	private static void doCheck(ColorSpace cs, int[] srgb) {
+		float[] srgb1 = {srgb[0]/255f, srgb[1]/255f, srgb[2]/255f};
+		float[] lab = cs.fromRGB(srgb1);
+		float[] srgb2 = cs.toRGB(lab);
+		assertArrayEquals("LinearRgb conversion problem for srgb=" + Arrays.toString(srgb), srgb1, srgb2, 1e-5f);
+	}
+
+}
