f60bb5b0e750a6324be28fb7957f687d9e76efef
imagingbook <wilbur@ieee.org>
2022-09-09 12:11:01 +0200

    minor edits

--- imagingbook-common/src/test/java/imagingbook/testutils/ImageResourceSelfTest.java
+++ imagingbook-common/src/test/java/imagingbook/testutils/ImageResourceSelfTest.java
@@ -2,18 +2,23 @@ package imagingbook.testutils;
 
 import org.junit.Test;
 
+import imagingbook.core.resource.ImageResource;
+import imagingbook.testimages.BinaryTestImage;
+
 /**
  * This class should be extended by some static class nested
  * inside a {@link ImageResource} enum type.
  * If so defined, the {@link ImageResource} will be tested automatically.
+ * See {@link BinaryTestImage}, for example.
  * 
  * @author WB
  */
 public abstract class ImageResourceSelfTest {
 	
 	/**
-	 * This method must be called from on an instance of a class nested inside 
+	 * This method must be called from on an instance of a public class nested inside 
 	 * an ImageResource enum type.
+	 * JUnit will instantiate this class and execute this test method.
 	 */
 	@Test
 	@SuppressWarnings({ "unchecked", "rawtypes" })
