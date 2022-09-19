8df727dc809e6342ae594021d0288755915b5df1
imagingbook <wilbur@ieee.org>
2022-09-19 10:08:34 +0200

    converted resources in FilterTestImage to autoName()

--- imagingbook-common/src/test/java/imagingbook/testimages/FilterTestImage.java
+++ imagingbook-common/src/test/java/imagingbook/testimages/FilterTestImage.java
@@ -20,53 +20,58 @@ import imagingbook.testutils.ImageResourceSelfTest;
  * @see GeneralSampleImage
  */
 public enum FilterTestImage implements ImageResource {
-	MonasterySmall("monastery-small.png"),
-	Clown("clown.png"),
+	MonasterySmall,
+	Clown,
 	
-	BoatsFilter3x3("boats-filter3x3.png"),
-	MonasterySmallFilter3x3("monastery-small-filter3x3.png"),
-	ClownFilter3x3("clown-filter3x3.png"),
+	BoatsFilter3x3,
+	MonasterySmallFilter3x3,
+	ClownFilter3x3,
 	
-	MonasterySmallGauss3("monastery-small-gauss3.png"),
-	ClownGauss3("clown-gauss3.png"),
+	MonasterySmallGauss3,
+	ClownGauss3,
 	
-	ClownExampleFilter3x3Scalar("clown-examplefilter3x3scalar.png"),
-	ClownExampleFilter3x3Vector("clown-examplefilter3x3vector.png"),
+	ClownExampleFilter3x3Scalar,
+	ClownExampleFilter3x3Vector,
 	
-	ClownMedianScalar3("clown-median-scalar-3.png"),
-	ClownMedianVector3L1("clown-median-vector-3-L1.png"),
-	ClownMedianVectorsharpen3L1("clown-median-vectorsharpen-3-L1.png"),
+	ClownMedianScalar3,
+	ClownMedianVector3L1,
+	ClownMedianVectorsharpen3L1,
 	
-	MonasterySmallBilateralNonsep("monastery-small-bilateral-nonsep.png"),
-	MonasterySmallBilateralSep("monastery-small-bilateral-sep.png"),
-	ClownBilateralNonsepScalar("clown-bilateral-nonsep-scalar.png"),
-	ClownBilateralNonsepVector("clown-bilateral-nonsep-vector.png"),
-	ClownBilateralSepScalar("clown-bilateral-sep-scalar.png"),
-	ClownBilateralSepVector("clown-bilateral-sep-vector.png"),
+	MonasterySmallBilateralNonsep,
+	MonasterySmallBilateralSep,
+	ClownBilateralNonsepScalar,
+	ClownBilateralNonsepVector,
+	ClownBilateralSepScalar,
+	ClownBilateralSepVector,
 	
-	MonasterySmallKuwahara("monastery-small-kuwahara.png"),
-	ClownKuwaharaScalar("clown-kuwahara-scalar.png"),
-	ClownKuwaharaVector("clown-kuwahara-vector.png"),
+	MonasterySmallKuwahara,
+	ClownKuwaharaScalar,
+	ClownKuwaharaVector,
 	
-	MonasterySmallNagaoMatsuyama("monastery-nagaomatsuyama.png"),
-	ClownNagaoMatsuyamaScalar("clown-nagaomatsuyama-scalar.png"),
-	ClownNagaoMatsuyamaVector("clown-nagaomatsuyama-vector.png"),
+	MonasterySmallNagaoMatsuyama,
+	ClownNagaoMatsuyamaScalar,
+	ClownNagaoMatsuyamaVector,
 	
-	MonasterySmallPeronaMalikG1("monastery-small-peronamalik-g1.png"),
-	MonasterySmallPeronaMalikG2("monastery-small-peronamalik-g2.png"),
-	MonasterySmallPeronaMalikG3("monastery-small-peronamalik-g3.png"),
-	MonasterySmallPeronaMalikG4("monastery-small-peronamalik-g4.png"),
-	ClownPeronaMalikSeparateChannels("clown-peronamalik-separatechannels.png"),
-	ClownPeronaMalikBrightnessGradient("clown-peronamalik-brightnessgradient.png"),
-	ClownPeronaMalikColorGradient("clown-peronamalik-colorgradient.png"),
+	MonasterySmallPeronaMalikG1,
+	MonasterySmallPeronaMalikG2,
+	MonasterySmallPeronaMalikG3,
+	MonasterySmallPeronaMalikG4,
 	
-	MonasterySmallTschumperleIter5("monastery-small-tschumperle-iter5.png"),
-	ClownSmallTschumperleIter5("clown-tschumperle-iter5.png"),
+	ClownPeronaMalikSeparateChannels,
+	ClownPeronaMalikBrightnessGradient,
+	ClownPeronaMalikColorGradient,
+	
+	MonasterySmallTschumperleIter5,
+	ClownSmallTschumperleIter5,
 	;
 	
 	// ---------------------------------------------------
 	
 	private final String filename;
+	
+	FilterTestImage() {
+		filename = null;
+	}
 	
 	FilterTestImage(String filename) {
 		this.filename = filename;
@@ -74,7 +79,10 @@ public enum FilterTestImage implements ImageResource {
 	
 	@Override
 	public String getFileName() {
-		return filename;
+		if (filename != null)
+			return filename;
+		else 
+			return this.autoName();
 	}
 	
 	// ---------------------------------------------------
---
+++ imagingbook-common/src/test/java/imagingbook/testutils/ResourceTestUtils.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * This software is provided as a supplement to the authors' textbooks on digital
+ * image processing published by Springer-Verlag in various languages and editions.
+ * Permission to use and distribute this software is granted under the BSD 2-Clause 
+ * "Simplified" License (see http://opensource.org/licenses/BSD-2-Clause). 
+ * Copyright (c) 2006-2022 Wilhelm Burger, Mark J. Burge. 
+ * All rights reserved. Visit https://imagingbook.com for additional details.
+ *******************************************************************************/
+package imagingbook.testutils;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import imagingbook.core.resource.ImageResource;
+import imagingbook.core.resource.NamedResource;
+
+/**
+ * Static methods for testing implementations of {@link NamedResource} and {@link ImageResource}.
+ * This class is duplicated in various projects since it is only available during the
+ * test phase and thus not contained in any dependency JAR. 
+ * 
+ * @author WB
+ */
+public abstract class ResourceTestUtils {
+	
+	/**
+	 * Scans all resources defined by some {@link NamedResource} class, validates that they exist.
+	 */
+	public static <E extends Enum<E>> void testNamedResource(Class<E> enumClass) {
+		assertTrue("enum must implement NamedResource", NamedResource.class.isAssignableFrom(enumClass));
+		if (NamedResource.class.isAssignableFrom(enumClass)) {
+			for (E item : enumClass.getEnumConstants()) {
+				NamedResource nr = (NamedResource) item;
+				assertNotNull("could not find resource " + item.toString(), nr.getURL());
+			}	
+		}
+	}
+	
+	/**
+	 * Scans all resources defined by some {@link ImageResource} class, validates that they can be opened
+	 * as images.
+	 */
+	public static <E extends Enum<E>> void testImageResource(Class<E> enumClass) {
+		assertTrue("enum must implement ImageResource", ImageResource.class.isAssignableFrom(enumClass));
+		if (ImageResource.class.isAssignableFrom(enumClass)) {
+			for (E item : enumClass.getEnumConstants()) {
+				ImageResource ir = (ImageResource) item;
+				File file = new File(ir.getRelativePath());
+				assertNotNull("could not find file " + file.getAbsolutePath(), ir.getURL());
+				assertNotNull("could not open image for resource " + ir,  ir.getImage());
+			}
+		}
+	}
+	
+}
