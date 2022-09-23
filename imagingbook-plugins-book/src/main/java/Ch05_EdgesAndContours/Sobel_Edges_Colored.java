6cbbcf876f1b9d4a50cb12409f259dcdd799732e
imagingbook <wilbur@ieee.org>
2022-09-23 11:54:38 +0200

    JavaDoc rebuild

--- imagingbook-plugins-book/src/main/java/Ch05_EdgesAndContours/Sobel_Edges_Colored.java
+++ imagingbook-plugins-book/src/main/java/Ch05_EdgesAndContours/Sobel_Edges_Colored.java
@@ -25,7 +25,7 @@ import ij.process.ImageProcessor;
  * The plugin displays the two derivative images ("Ix", "Iy"),
  * edge magnitude and orientation ("E", "phi") and
  * color-coded edge magnitude ("E colors", hue controlled by edge orientation).
- * Edge magnitude is &ge; 0, orientation is in [&minus;&pi;,&plus;&pi;].
+ * Edge magnitude is &ge; 0, orientation is in [&minus;&pi;,+&pi;].
  * See Sec. 5.3.1 of [1] for additional details.
  * </p>
  * <p>
