6dc3eb2e5b83fbb201cf21bfaff1fcb98975b1c5
imagingbook <wilbur@ieee.org>
2022-09-29 11:38:27 +0200

    Make_Projections: completely revised to match book examples using 'Projection' library class, user dialog added

--- imagingbook-plugins-book/src/main/java/Ch08_BinaryRegions/Make_Projections.java
+++ imagingbook-plugins-book/src/main/java/Ch08_BinaryRegions/Make_Projections.java
@@ -9,18 +9,32 @@
 package Ch08_BinaryRegions;
 
 import ij.ImagePlus;
+import ij.gui.GenericDialog;
 import ij.plugin.filter.PlugInFilter;
-import ij.process.ByteProcessor;
+import ij.process.ColorProcessor;
 import ij.process.ImageProcessor;
+import imagingbook.common.color.sets.BasicAwtColor;
+import imagingbook.common.image.Projection;
 import imagingbook.core.plugin.IjPluginName;
 
 /**
  * <p>
  * This ImageJ plugin demonstrates the calculation of horizontal and vertical
- * projections on a grayscale image. See Sec. 8.7 of [1] for additional details.
- * The resulting projections are shown as separate images, the original image is
- * not modified.
+ * projections from a grayscale image. See Sec. 8.7 of [1] for additional
+ * details. The input image is not assumed binary, i.e., gray values are simply
+ * added up without thresholding. This plugin only works for 8-bit grayscale
+ * images, the original image is not modified. The resulting projections are
+ * shown as separate diagrams, which either show the relative amount of white
+ * (default) or black, selectable in the user dialog:
  * </p>
+ * <ul>
+ * <li>The <em>horizontal</em> projection diagram has the same height as the
+ * original image and user-adjustable width, with diagram bars being drawn from
+ * left to right.</li>
+ * <li>The <em>vertical</em> projection diagram has the same width as the
+ * original image and user-adjustable height, with diagram bars being drawn from
+ * top to bottom.</li>
+ * </ul>
  * <p>
  * [1] W. Burger, M.J. Burge, <em>Digital Image Processing - An Algorithmic
  * Approach</em>, 3rd ed, Springer (2022).
@@ -28,16 +42,27 @@ import imagingbook.core.plugin.IjPluginName;
  * 
  * @author WB
  * @version 2020/12/17
+ * @version 2022/09/29 revised to match book examples
+ * 
+ * @see imagingbook.common.image.Projection
  */
 @IjPluginName("Make Projections")
 public class Make_Projections implements PlugInFilter {
 	
 	/** Size of horizontal/vertical projection bars. */
-	static final int PSIZE = 150;
-	/** Foreground value for drawing (black). */
-	static final int FOREGROUND = 0;
-	/** Background value for drawing (white). */
-	static final int BACKGROUND = 255;
+	public static int ProjectionSize = 150;
+	
+	/** Background color for projection diagrams. */
+	public static BasicAwtColor BackgroundColor = BasicAwtColor.White;
+	
+	/** Foreground value for horizontal projection. */
+	public static BasicAwtColor PlotColorH = BasicAwtColor.Green;
+	
+	/** Foreground value for vertical projection. */
+	public static BasicAwtColor PlotColorV = BasicAwtColor.Magenta;
+	
+	/** Plot the relative amount of white (false) or black (true). */
+	public static boolean ShowAmountOfBlack = false;
 	
 	ImagePlus im = null;
 	
@@ -48,46 +73,72 @@ public class Make_Projections implements PlugInFilter {
 	}
 	
 	@Override
-	public void run(ImageProcessor I) {
-		int M = I.getWidth();
-		int N = I.getHeight();
-		int[] pHor = new int[N];
-		int[] pVer = new int[M];
-		for (int v = 0; v < N; v++) {
-			for (int u = 0; u < M; u++) {
-				int p = I.getPixel(u, v);
-				pHor[v] +=  p;
-				pVer[u] +=  p;
-			}
+	public void run(ImageProcessor ip) {
+		
+		if (!runDialog()) {
+			return;
 		}
 		
-		// make the horizontal projection:
-		ByteProcessor hP = new ByteProcessor(PSIZE,N);
-		hP.setValue(BACKGROUND); hP.fill();
-		hP.setValue(FOREGROUND);
+		String title = im.getShortTitle();
+		int M = ip.getWidth();
+		int N = ip.getHeight();
+		
+		Projection proj = new Projection(ip);
+		
+		// draw the horizontal projection diagram:
+		double[] pHor = proj.getHorizontal();	// pHor[v] = sum of pixel values in row v
+		ColorProcessor hP = new ColorProcessor(ProjectionSize, N);
+		hP.setColor(BackgroundColor.getColor());
+		hP.fill();
+		hP.setColor(PlotColorH.getColor());
 		double maxWhite = M * 255;
-		for (int row = 0; row < N; row++) {
-			double black = (1 - pHor[row] / maxWhite);	// percentage of black in line 'row'
-			int k = (int) (black * PSIZE);
+		for (int v = 0; v < N; v++) {
+			double amount = (ShowAmountOfBlack) ?
+					1 - pHor[v] / maxWhite :	// amount of black in line 'v'
+					pHor[v] / maxWhite;			// amount of white in line 'v'
+			int k = (int) Math.round(amount * ProjectionSize);
 			if (k > 0) {
-				hP.drawLine(0, row, k, row);
+				hP.drawLine(0, v, k, v);
 			}
-		}
-		(new ImagePlus("Horizontal projection", hP)).show();
+		}	
+		(new ImagePlus(title + "-horizontal-proj", hP)).show();
 				
-		// make the vertical projection:
-		ByteProcessor vP = new ByteProcessor(M,PSIZE);
-		vP.setValue(BACKGROUND); vP.fill();
-		vP.setValue(FOREGROUND);
+		// draw the vertical projection diagram:
+		double[] pVer = proj.getVertical();		// pVer[u] = sum of pixel values in column u
+		ColorProcessor vP = new ColorProcessor(M, ProjectionSize);
+		vP.setColor(BackgroundColor.getColor()); 
+		vP.fill();
+		vP.setColor(PlotColorV.getColor());
 		maxWhite = N * 255;
-		for (int col = 0; col < M; col++) {
-			double black = (1 - pVer[col] / maxWhite);	// percentage of black in column 'col'
-			int k = (int) (black * PSIZE);
+		for (int u = 0; u < M; u++) {
+			double amount = (ShowAmountOfBlack) ?
+					1 - pVer[u] / maxWhite :	// amount of black in column 'u'
+					pVer[u] / maxWhite;			// amount of white in column 'u'
+			int k = (int) (amount * ProjectionSize);
 			if (k > 0) {
-				vP.drawLine(col, 0, col, k);
+				vP.drawLine(u, 0, u, k);
 			}
 		}
-		(new ImagePlus("Vertical projection", vP)).show();
+		(new ImagePlus(title + "-vertical-proj", vP)).show();
 	}
 
+	private boolean runDialog() {
+		GenericDialog gd = new GenericDialog(this.getClass().getSimpleName());
+		gd.addNumericField("Size of projection plots", ProjectionSize, 0);
+		gd.addCheckbox("Show amount of black", ShowAmountOfBlack);
+		gd.addEnumChoice("Horizontal plot color", PlotColorH);
+		gd.addEnumChoice("Vertical plot color", PlotColorV);
+		gd.addEnumChoice("Plot background color", BackgroundColor);
+		
+		gd.showDialog();
+		if (gd.wasCanceled())
+			return false;
+		
+		ProjectionSize = (int) gd.getNextNumber();
+		ShowAmountOfBlack = gd.getNextBoolean();
+		PlotColorH = gd.getNextEnumChoice(BasicAwtColor.class);
+		PlotColorV = gd.getNextEnumChoice(BasicAwtColor.class);
+		BackgroundColor = gd.getNextEnumChoice(BasicAwtColor.class);
+		return true;
+	}
 }
