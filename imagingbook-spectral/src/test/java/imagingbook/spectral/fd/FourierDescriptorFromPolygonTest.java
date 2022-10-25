29e873ccbad187c23da3ad0390067092e89d4c20
imagingbook <wilbur@ieee.org>
2022-10-25 10:42:39 +0200

    more FourierDescriptor tests

--- imagingbook-spectral/src/test/java/imagingbook/spectral/fd/FourierDescriptorFromPolygonTest.java
+++ imagingbook-spectral/src/test/java/imagingbook/spectral/fd/FourierDescriptorFromPolygonTest.java
@@ -9,7 +9,7 @@ import imagingbook.common.geometry.basic.PntUtils;
 import imagingbook.common.math.Complex;
 import imagingbook.spectral.TestUtils;
 
-public class FourierDescriptorUniformTest {
+public class FourierDescriptorFromPolygonTest {
 
 	static double TOL = 1e-6;
 	
@@ -18,28 +18,27 @@ public class FourierDescriptorUniformTest {
 		Pnt2d[] V = PntUtils.makeDoublePoints(3,2, 5,4, 7,10, 6,11, 4,7);
 		
 		Complex[] c1 = {	// DFT coefficients
-				new Complex(5.000000000, 6.800000000), 
-				new Complex(-1.635404556, -2.625802342), 
-				new Complex(-0.215246253, -0.009311758), 
-				new Complex(0.109673444, -0.154620264), 
-				new Complex(-0.259022635, -2.010265635)};
+				new Complex(4.899143645, 6.495130576), 
+				new Complex(-0.932154980, -2.315184973), 
+				new Complex(0.001385310, 0.033226034), 
+				new Complex(-0.030610108, -0.033017669), 
+				new Complex(-0.470565980, -1.415652554)};
 		
-		FourierDescriptor fd1 = new FourierDescriptorUniform(V); 
+		FourierDescriptor fd1 = new FourierDescriptorFromPolygon(V, 2);
 //		System.out.println(Arrays.toString(fd1.getCoefficients()));
 		assertEquals(5, fd1.size());
 		TestUtils.assertArrayEquals(c1, fd1.getCoefficients(), TOL);
 		
-		FourierDescriptor fd2 = new FourierDescriptorUniform(V, 2); 
+		FourierDescriptor fd2 = new FourierDescriptorFromPolygon(V, 2);
 //		System.out.println(Arrays.toString(fd2.getCoefficients()));
 		assertEquals(5, fd2.size());
 		Complex[] c2 = c1;
 		TestUtils.assertArrayEquals(c2, fd2.getCoefficients(), TOL);
 		
-		FourierDescriptor fd3 = new FourierDescriptorUniform(V, 1); 
+		FourierDescriptor fd3 = new FourierDescriptorFromPolygon(V, 1); 
 //		System.out.println(Arrays.toString(fd3.getCoefficients()));
 		assertEquals(3, fd3.size());
 		Complex[] c3 = {c1[0], c1[1], c1[4]};
 		TestUtils.assertArrayEquals(c3, fd3.getCoefficients(), TOL);
 	}
-	
 }
