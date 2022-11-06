faf1ced71e1dffba2aa2d1955c0be0c92e558780
imagingbook <wilbur@ieee.org>
2022-11-06 16:46:40 +0100

    Desaturate_RGB plugin: renamed, dialog and JavaDoc added

--- imagingbook-plugins-book/src/main/java/Ch13_ColorImages/Desaturate_RGB.java
+++ imagingbook-plugins-book/src/main/java/Ch13_ColorImages/Desaturate_RGB.java
@@ -9,13 +9,17 @@
 package Ch13_ColorImages;
 
 import ij.ImagePlus;
+import ij.gui.GenericDialog;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
 
 /**
  * <p>
- * ImageJ plugin, continuously desaturates the current RGB color
- * image. See Sec. 13.2.2 (Prog. 13.5) of [1] for details.
+ * ImageJ plugin, continuously desaturates the current image directly in RGB
+ * color space (without conversion to HSV or HSB). The degree of desaturation is
+ * controlled by factor S &in; [0,1]. With S = 1, the image is unchanged, with S
+ * = 0 the resulting image is completely colorless (gray). See Sec. 13.2.2
+ * (Prog. 13.5) of [1] for details.
  * </p>
  * <p>
  * [1] W. Burger, M.J. Burge, <em>Digital Image Processing &ndash; An
@@ -24,9 +28,9 @@ import ij.process.ImageProcessor;
  * 
  * @author WB
  */
-public class Desaturate_Rgb implements PlugInFilter {
+public class Desaturate_RGB implements PlugInFilter {
 	
-	static double S = 0.3; // color saturation value
+	public static double S = 0.3; // color saturation factor
 
 	@Override
 	public int setup(String arg, ImagePlus imp) {
@@ -34,7 +38,12 @@ public class Desaturate_Rgb implements PlugInFilter {
 	}
 	
 	@Override
-	public void run(ImageProcessor ip) { 
+	public void run(ImageProcessor ip) {
+		
+		if (!runDialog()) {
+			return;
+		}
+		
 		final int w = ip.getWidth();
 		final int h = ip.getHeight();
 		
@@ -42,27 +51,49 @@ public class Desaturate_Rgb implements PlugInFilter {
 		for (int v = 0; v < h; v++) {
 			for (int u = 0; u < w; u++) {
 
-				//get int-packed color pixel
+				// get int-packed color pixel
 				int c = ip.get(u, v);
 
-				//extract RGB components from color pixel
+				// disassemble RGB components from int color pixel
 				int r = (c & 0xff0000) >> 16;
 				int g = (c & 0x00ff00) >> 8;
 				int b = (c & 0x0000ff);
 
-				//compute equivalent gray value
+				//compute equivalent gray value (luma, ITU BR.601 weights)
 				double y = 0.299 * r + 0.587 * g + 0.114 * b;
 
 				// linearly interpolate $(yyy) --> (rgb)
-				r = (int) (y + S * (r - y));
-				g = (int) (y + S * (g - y));
-				b = (int) (y + S * (b - y));
+				r = clamp(y + S * (r - y));
+				g = clamp(y + S * (g - y));
+				b = clamp(y + S * (b - y));
 
-				// reassemble color pixel
-				c = ((r & 0xff)<<16) | ((g & 0xff)<<8) | b & 0xff; 
+				// reassemble color pixel to int
+				c = ((r & 0xff) << 16) | ((g & 0xff) << 8) | b & 0xff; 
 				ip.set(u, v, c);
 			}
 		}
+	}
+	
+	private int clamp(double x) {
+		int z = (int) Math.round(x);
+		if (z < 0) return 0;
+		if (z > 255) return 255;
+		return z;
+	}
+	
+	private boolean runDialog() {
+		GenericDialog gd = new GenericDialog(this.getClass().getSimpleName());
+		gd.addMessage("Saturates or desaturates the current image.");
+		gd.addNumericField("Saturation factor (S = 0..1)", S, 2);
+		
+		gd.showDialog();
+		if (gd.wasCanceled())
+			return false;
+		
+		S = gd.getNextNumber();
+		if (S < 0) S = 0;
+		if (S > 1) S = 1;
+		return true;
 	}
 
 }
