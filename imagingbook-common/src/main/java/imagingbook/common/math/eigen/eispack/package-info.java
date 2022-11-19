5cc26aa511abd3ae9b56bc822708f0c0d444bbd0
imagingbook <wilbur@ieee.org>
2022-11-19 10:15:58 +0100

    added package-info to eispack

---
+++ imagingbook-common/src/main/java/imagingbook/common/math/eigen/eispack/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Original EISPACK code (FORTRAN) for solving generalized eigenvalue problems, 
+ * copied from http://www.netlib.no/netlib/eispack/ .
+ */
+package imagingbook.common.math.eigen.eispack;
