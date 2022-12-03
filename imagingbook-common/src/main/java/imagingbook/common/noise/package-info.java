c56485fc31b2a49e173a70c8a8214f3b95c2bd9b
imagingbook <wilbur@ieee.org>
2022-12-03 10:16:21 +0100

    source copyright note updated

--- imagingbook-common/src/main/java/imagingbook/common/noise/package-info.java
+++ imagingbook-common/src/main/java/imagingbook/common/noise/package-info.java
@@ -1,4 +1,13 @@
 
+/*
+ *  This software is provided as a supplement to the authors' textbooks on digital
+ * image processing published by Springer-Verlag in various languages and editions.
+ * Permission to use and distribute this software is granted under the BSD 2-Clause
+ * "Simplified" License (see http://opensource.org/licenses/BSD-2-Clause).
+ * Copyright (c) 2006-2022 Wilhelm Burger, Mark J. Burge.
+ * All rights reserved. Visit https://imagingbook.com for additional details.
+ */
+
 /**
  * <p>
  * The code in this package relates to the materials discussed in [1].
