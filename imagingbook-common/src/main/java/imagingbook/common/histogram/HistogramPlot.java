4b746c42e1a43e1725da760c5342d2bfd19694e6
imagingbook <wilbur@ieee.org>
2022-12-07 12:21:31 +0100

    revised/simplified HistogramPlot

--- imagingbook-common/src/main/java/imagingbook/common/histogram/HistogramPlot.java
+++ imagingbook-common/src/main/java/imagingbook/common/histogram/HistogramPlot.java
@@ -10,71 +10,56 @@
 package imagingbook.common.histogram;
 
 import ij.ImagePlus;
-import ij.gui.NewImage;
-import ij.process.ImageProcessor;
+import ij.process.ByteProcessor;
 
 /**
- * Defines a simple image window to display histograms. This is a sub-class of
- * {@link ImagePlus} (similar to {@code ij.gui.HistogramPlot}).
- * 
+ * Defines a simple image window to display histograms. This is a sub-class of {@link ImagePlus} (similar to
+ * {@code ij.gui.HistogramPlot}).
+ *
  * @author WB
- * @version 2022/09/22 revised to extend ImagePlus
+ * @version 2022/12/07
  */
 public class HistogramPlot extends ImagePlus {
 	
-	static final int BACKGROUND = NewImage.FILL_WHITE;
-
-    private final int width =  256;
-    private final int height = 128;
-    private final int base = height - 1;
-    private final int paintValue = 0;
-	private final int[] H = new int[256];
+	private static final int Background = 255;
+	private static final int Foreground = 0;
+    private static final int width =  256;
+    private static final int height = 128;
 	
 	/**
 	 * Constructor for a normalized discrete distribution.
 	 * @param nH a normalized discrete distribution with values in [0,1]
-	 * @param title the window title to be displayed 
+	 * @param title the window title to be displayed (may be null)
 	 */
 	public HistogramPlot(double[] nH, String title) {
-		setImage(NewImage.createByteImage(title, width, height, 1, BACKGROUND));
-		// nH must be a normalized histogram of length 256
-		for (int i = 0; i < nH.length; i++) {
-			H[i] = (int) Math.round(height * nH[i]);
-		}
-		drawHist();
+		super(title, drawHistogram(nH));
 	}
 	
 	/**
 	 * Constructor for a discrete distribution.
 	 * @param h a discrete distribution (histogram) with arbitrary values
-	 * @param title the window title to be displayed 
+	 * @param title the window title to be displayed (may be null)
 	 */
 	public HistogramPlot(int[] h, String title) {
 		this(HistogramUtils.normalizeMax(h), title);
 	}
-	
-	/**
-	 * Constructor for a piecewise linear cumulative distribution.
-	 * @param cdf a piecewise linear cumulative distribution function
-	 * @param title the window title to be displayed 
-	 */
-	public HistogramPlot(PiecewiseLinearCdf cdf, String title) {
-		this(cdf.getCdf(), title);
-	}
-	
-	private void drawHist() {
-		ImageProcessor ip = this.getProcessor();
-		ip.setValue(0);
+
+	private static ByteProcessor drawHistogram(double[] nH) {
+		ByteProcessor ip = new ByteProcessor(width, height);
+		int base = height - 1;
+		ip.setValue(Background);
+		ip.fill();
+		ip.setValue(Foreground);
 		ip.drawLine(0, base, width - 1, base);
-		ip.setValue(paintValue);
 		int u = 0;
-		for (int i = 0; i < H.length; i++) {
-			int k = H[i];
+		for (int i = 0; i < nH.length; i++) {
+			int k = (int) Math.round(height * nH[i]);
 			if (k > 0) {
 				ip.drawLine(u, base - 1, u, base - k);
 			}
 			u = u + 1;
 		}
+		return ip;
 	}
 	
 }
