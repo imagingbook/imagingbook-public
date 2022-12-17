e087e8bf60c5e8062dcf982a05f9d2c7f1cf116b
imagingbook <wilbur@ieee.org>
2022-12-17 09:28:27 +0100

    Copyright update

--- imagingbook-common/src/main/java/imagingbook/common/color/adapt/BradfordAdaptation.java
+++ imagingbook-common/src/main/java/imagingbook/common/color/adapt/BradfordAdaptation.java
@@ -1,11 +1,11 @@
-/*
- *  This software is provided as a supplement to the authors' textbooks on digital
+/*******************************************************************************
+ * This software is provided as a supplement to the authors' textbooks on digital
  * image processing published by Springer-Verlag in various languages and editions.
  * Permission to use and distribute this software is granted under the BSD 2-Clause
  * "Simplified" License (see http://opensource.org/licenses/BSD-2-Clause).
- * Copyright (c) 2006-2022 Wilhelm Burger, Mark J. Burge.
- * All rights reserved. Visit https://imagingbook.com for additional details.
- */
+ * Copyright (c) 2006-2022 Wilhelm Burger, Mark J. Burge. All rights reserved.
+ * Visit https://imagingbook.com for additional details.
+ ******************************************************************************/
 
 package imagingbook.common.color.adapt;
 
@@ -16,20 +16,16 @@ import imagingbook.common.color.cie.Illuminant;
 
 /**
  * <p>
- * This class represents a linear chromatic adaptation transform,
- * mapping XYZ color coordinates from a source white (reference) point
- * to a target white point. 
- * Both white points are passed to the constructor.
- * The actual color mapping is done by method {@link #applyTo(float[])}.
- * The underlying linear transformation is specified by a 3x3 matrix,
- * which may be retrieved by {@link #getAdaptationMatrix()}).
- * See Sec. 14.6 of [1] for additional details and examples.
+ * This class represents a linear chromatic adaptation transform, mapping XYZ color coordinates from a source white
+ * (reference) point to a target white point. Both white points are passed to the constructor. The actual color mapping
+ * is done by method {@link #applyTo(float[])}. The underlying linear transformation is specified by a 3x3 matrix, which
+ * may be retrieved by {@link #getAdaptationMatrix()}). See Sec. 14.6 of [1] for additional details and examples.
  * </p>
  * <p>
- * [1] W. Burger, M.J. Burge, <em>Digital Image Processing &ndash; 
- * An Algorithmic Introduction</em>, 3rd ed, Springer (2022).
+ * [1] W. Burger, M.J. Burge, <em>Digital Image Processing &ndash; An Algorithmic Introduction</em>, 3rd ed, Springer
+ * (2022).
  * </p>
- * 
+ *
  * @author WB
  * @version 2022/11/14
  * @see XYZscalingAdaptation
@@ -55,23 +51,21 @@ public class BradfordAdaptation implements ChromaticAdaptation {
 	private final double[][] Mdiag;
 	
 	// ---------------------------------------------------------------------------------
-	
+
 	/**
-	 * Returns a {@link BradfordAdaptation} instance for the specified
-	 * white point coordinates.
-	 * 
+	 * Returns a {@link BradfordAdaptation} instance for the specified white point coordinates.
+	 *
 	 * @param W1 source white point (to map from)
 	 * @param W2 target white point (to map to)
-	 * @return a {@link BradfordAdaptation} instance 
+	 * @return a {@link BradfordAdaptation} instance
 	 */
 	public static BradfordAdaptation getInstance(double[] W1, double[] W2) {
 		return new BradfordAdaptation(W1, W2);
 	}
-	
+
 	/**
-	 * Returns a {@link BradfordAdaptation} instance for the specified
-	 * illuminants (white points).
-	 * 
+	 * Returns a {@link BradfordAdaptation} instance for the specified illuminants (white points).
+	 *
 	 * @param illum1 source illuminant (white point to map from)
 	 * @param illum2 target illuminant (white point to map to)
 	 * @return a {@link BradfordAdaptation} instance
