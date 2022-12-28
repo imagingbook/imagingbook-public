e1da4fd22e3f70a3b12bd0e6228af4478102c9a9
imagingbook <wilbur@ieee.org>
2022-12-28 17:30:54 +0100

    added KimiaCollage (1 test image)

---
+++ imagingbook-sample-images/src/main/java/imagingbook/sampleimages/kimia/KimiaCollage.java
@@ -0,0 +1,16 @@
+package imagingbook.sampleimages.kimia;
+
+import imagingbook.core.resource.ImageResource;
+
+/**
+ * Test images composed of shapes from the Kimia data sets (see https://vision.lems.brown.edu/content/available-software-and-databases)
+ *
+ * @author WB
+ * @see Kimia216
+ * @see Kimia1070
+ */
+public enum KimiaCollage implements ImageResource {
+    ShapeCollage1
+    ;
+
+}
