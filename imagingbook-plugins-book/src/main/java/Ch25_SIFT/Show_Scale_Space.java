d8f324f6773e6bcb7bf422162058b03e320c9250
imagingbook <wilbur@ieee.org>
2023-01-08 10:54:38 +0100

    reverted

--- imagingbook-plugins-book/src/main/java/Ch25_SIFT/Show_Scale_Space.java
+++ imagingbook-plugins-book/src/main/java/Ch25_SIFT/Show_Scale_Space.java
@@ -41,25 +41,6 @@ import static imagingbook.common.ij.IjUtils.noCurrentImage;
  */
 public class Show_Scale_Space implements PlugInFilter {
 
-	private static String HelpText = makeHtmlString(
-		"This ImageJ plugin visualizes the hierarchical scale space structures",
-		"used for SIFT feature detection. Optionally the Gaussian scale space or the",
-		"derived DoG scale space (or both) are shown. Each scale space octave",
-		"is displayed as an image stack, with one frame for each scale level."
-	);
-
-
-
-	private static String makeHelpString(String... lines) {
-		String joined = String.join(" ", lines);
-		String broken = DialogUtils.splitLines(40, joined);
-
-		String text = "<html>\n" + joined + "\n</html>";
-		// String text = "<html>\n" + DialogUtils.splitLines(20, lines) + "\n</html>";
-		IJ.log(text);
-		return text;
-	}
-
 	private static ImageResource SampleImage = GeneralSampleImage.Castle;
 	private static boolean ShowGaussianScaleSpace = true;
 	private static boolean ShowDoGScaleSpace = false;
@@ -113,7 +94,6 @@ public class Show_Scale_Space implements PlugInFilter {
 	
 	private boolean runDialog() {
 		GenericDialog gd = new GenericDialog(this.getClass().getSimpleName());
-		gd.addHelp(HelpText);
 		gd.addCheckbox("Show Gaussian scale space)", ShowGaussianScaleSpace);
 		gd.addCheckbox("Show DoG scale space)", ShowDoGScaleSpace);
 		
@@ -125,62 +105,6 @@ public class Show_Scale_Space implements PlugInFilter {
 		ShowGaussianScaleSpace = gd.getNextBoolean();
 		ShowDoGScaleSpace = gd.getNextBoolean();
 		return true;
-	}
-
-	// EXPERIMENTAL!  -------------------------------------------------------------------------
-	private static final String LINEBREAK = "<br>\n";
-	private static final String SPACE_SEPARATOR = " ";
-	//if text has \n, \r or \t symbols it's better to split by \s+
-	private static final String SPLIT_REGEXP= "\\s+";
-
-	/**
-	 * Splits a long string into multiple lines of the specified maximum length and builds a new string with newline
-	 * ({@literal \n}) characters separating successive lines. Multiple input strings are first joined into a single
-	 * string using blank spaces as separators. Intended mainly to format message texts of plugin dialogs. Inspired by:
-	 * https://stackoverflow.com/a/21002193
-	 *
-	 * @param columns the maximum number of characters per line
-	 * @param input the input text to be formatted
-	 * @return a new string with newline characters separating successive lines
-	 */
-	private static String formatText(int columns, String input) {
-		// if (textChunks.length == 0) {
-		// 	throw new IllegalArgumentException("must pass at least one string");
-		// }
-		// String input = String.join(SPACE_SEPARATOR, textChunks);
-		String[] tokens = input.split(SPLIT_REGEXP);
-		StringBuilder output = new StringBuilder(input.length());
-		int lineLen = 0;
-		for (int i = 0; i < tokens.length; i++) {
-			String word = tokens[i];
-
-			if (lineLen + SPACE_SEPARATOR.length() + word.length() > columns) {
-				if (i > 0) {
-					output.append(LINEBREAK);
-				}
-				lineLen = 0;
-			}
-			if (i < tokens.length - 1 &&
-				(lineLen + word.length() + SPACE_SEPARATOR.length() + tokens[i + 1].length() <= columns)) {
-				word += SPACE_SEPARATOR;
-			}
-			output.append(word);
-			lineLen += word.length();
-		}
-		return output.toString();
-	}
-
-	public static void main(String[] args) {
-		String[] lines = {
-				"This ImageJ plugin visualizes the hierarchical scale space structures",
-				"used for SIFT feature detection. Optionally the Gaussian scale space or the",
-				"derived DoG scale space (or both) are shown. Each scale space octave",
-				"is displayed as an image stack, with one frame for each scale level."};
-
-		String joined = String.join(" ", lines);
-		System.out.println("joined = \n" + joined);
-		String broken = formatText(60, joined);
-		System.out.println("broken = \n" + broken);
 	}
 
 }
