4f152f96f876b7f7ed074051caeea388f1518054
imagingbook <wilbur@ieee.org>
2023-01-11 09:46:55 +0100

    updated copyright headers

--- imagingbook_plugins_book/src/main/java/Ch05_Edges_Contours/package-info.java
+++ imagingbook_plugins_book/src/main/java/Ch05_Edges_Contours/package-info.java
@@ -1,13 +1,12 @@
-/**
- * This software is provided as a supplement to the authors' textbooks on digital image processing published by
- * Springer-Verlag in various languages and editions. Permission to use and distribute this software is granted under
- * the BSD 2-Clause "Simplified" License (see http://opensource.org/licenses/BSD-2-Clause). Copyright (c) 2006-2023
- * Wilhelm Burger, Mark J. Burge. All rights reserved. Visit https://imagingbook.com for additional details.
- */
+/*******************************************************************************
+ * This software is provided as a supplement to the authors' textbooks on digital
+ * image processing published by Springer-Verlag in various languages and editions.
+ * Permission to use and distribute this software is granted under the BSD 2-Clause
+ * "Simplified" License (see http://opensource.org/licenses/BSD-2-Clause).
+ * Copyright (c) 2006-2023 Wilhelm Burger, Mark J. Burge. All rights reserved.
+ * Visit https://imagingbook.com for additional details.
+ ******************************************************************************/
 
-/**
- *
- */
 @IjPluginPath("B&B>Book>Ch05 Edges & Contours")
 @JavaDocBaseUrl("https://imagingbook.github.io/imagingbook-public/javadoc/imagingbook_plugins_book/")
 package Ch05_Edges_Contours;
