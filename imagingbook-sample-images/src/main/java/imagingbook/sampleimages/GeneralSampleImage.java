babd7b58d56f21af3d2274e6f4bf3b1b94c94652
imagingbook <wilbur@ieee.org>
2025-10-24 10:27:22 +0200

    Updated copyright info in source files.

--- imagingbook-sample-images/src/main/java/imagingbook/sampleimages/GeneralSampleImage.java
+++ imagingbook-sample-images/src/main/java/imagingbook/sampleimages/GeneralSampleImage.java
@@ -3,7 +3,7 @@
  * image processing published by Springer-Verlag in various languages and editions.
  * Permission to use and distribute this software is granted under the BSD 2-Clause
  * "Simplified" License (see http://opensource.org/licenses/BSD-2-Clause).
- * Copyright (c) 2006-2023 Wilhelm Burger, Mark J. Burge. All rights reserved.
+ * Copyright (c) 2006-2025 Wilhelm Burger, Mark J. Burge. All rights reserved.
  * Visit https://imagingbook.com for additional details.
  ******************************************************************************/
 package imagingbook.sampleimages;
