1894b94d7b6b502a72d7558664a8d6d198ad9ea4
imagingbook <wilbur@ieee.org>
2025-12-13 16:47:12 +0100

    Added methods for generating primitive arrays by evaluating lambda expressions.

--- imagingbook-common/src/main/java/imagingbook/common/util/ArrayUtils.java
+++ imagingbook-common/src/main/java/imagingbook/common/util/ArrayUtils.java
@@ -10,13 +10,16 @@ package imagingbook.common.util;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.Random;
+import java.util.function.DoubleSupplier;
+import java.util.function.IntSupplier;
 
 /**
  * @author WB
  * @version 2022/11/20
  *
  */
-public abstract class ArrayUtils {
+public final class ArrayUtils {
 	
 	private ArrayUtils() {}
 	
@@ -46,4 +49,87 @@ public abstract class ArrayUtils {
 	public static <T> Iterator<T> getIterator(T[] array) {
 		return Arrays.stream(array).iterator();
 	}
+
+	// Primitive array creation: -----------------------------------------------
+	// TODO: add tests!
+
+	/**
+	 * Substitute for {@code java.util.function.FloatSupplier} missing from
+	 * standard JDK.
+	 */
+	@FunctionalInterface
+	public interface FloatSupplier {
+		float get();
+	}
+
+	/**
+	 * Utility method to create a {@code float} array with the specified length,
+	 * using a lambda expression to initialize each element. Usage example:
+	 * <pre>
+	 * float[] floats = newFloatArray(5, () -> (float) Math.random());
+	 * </pre>
+	 * @param length the length of the new array
+	 * @param supplier a supplier (lambda) expression
+	 * @return a new {@code float} array
+	 */
+	public static float[] newFloatArray(int length, FloatSupplier supplier) {
+		float[] result = new float[length];
+		for (int i = 0; i < length; i++) {
+			result[i] = supplier.get();
+		}
+		return result;
+	}
+
+	/**
+	 * Utility method to create a {@code int} array with the specified length,
+	 * using a lambda expression to initialize each element. Usage example:
+	 * <pre>
+	 * Random rg = new Random();
+	 * int[] integers = newIntArray(7,() ->  rg.nextInt(100) - 50);
+	 * </pre>
+	 * @param length the length of the new array
+	 * @param supplier a supplier (lambda) expression
+	 * @return a new {@code int} array
+	 */
+	public static int[] newIntArray(int length, IntSupplier supplier) {
+		int[] result = new int[length];
+		for (int i = 0; i < length; i++) {
+			result[i] = supplier.getAsInt();
+		}
+		return result;
+	}
+
+	/**
+	 * Utility method to create a {@code double} array with the specified length,
+	 * using a lambda expression to initialize each element. Usage example:
+	 * <pre>
+	 * double[] doubles = newDoubleArray(3, Math::random);
+	 * </pre>
+	 * @param length the length of the new array
+	 * @param supplier a supplier (lambda) expression
+	 * @return a new {@code double} array
+	 */
+	public static double[] newDoubleArray(int length, DoubleSupplier supplier) {
+		double[] result = new double[length];
+		for (int i = 0; i < length; i++) {
+			result[i] = supplier.getAsDouble();
+		}
+		return result;
+	}
+
+	// ----------------------------------------------
+
+	public static void main(String[] args) {
+		float[] floats = newFloatArray(5, () -> (float) Math.random());
+		System.out.println(Arrays.toString(floats));
+
+		double[] doubles = newDoubleArray(3, Math::random);
+		System.out.println(Arrays.toString(doubles));
+
+		Random rg = new Random();
+		int[] integers = newIntArray(7,() ->  rg.nextInt(100) - 50);
+		System.out.println(Arrays.toString(integers));
+
+	}
+
 }
